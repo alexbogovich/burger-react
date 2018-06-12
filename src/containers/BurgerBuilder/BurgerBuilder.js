2908f36aa500c0dd1b76b8430ae817531115d3ca
Alex Bogovich <bogovich.alexander@gmail.com>
2018-06-12 16:28:07 +0700

    Add More and Less actions

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Component, Fragment } from 'react'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+}
+
 class BurgerBuilder extends Component {
   
   state = {
@@ -9,15 +16,45 @@ class BurgerBuilder extends Component {
       salad: 1,
       bacon: 1,
       cheese: 2,
-      meat: 2
+      meat: 2,
+    },
+    totalPrice: 4,
+  }
+  
+  addIngredientHandler = (type) => {
+    const newCount = this.state.ingredients[type] + 1
+    const updatedIngredients = {
+      ...this.state.ingredients
     }
+    updatedIngredients[type] = newCount
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type]
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+  }
+  
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type]
+    if (oldCount <= 0 ) return
+    const newCount = oldCount - 1
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = newCount
+    const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type]
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
   }
   
   render () {
+    const disabledInfo = {...this.state.ingredients}
+    for (let key in disabledInfo)
+      disabledInfo[key] = disabledInfo[key] <= 0
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls/>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+        />
       </Fragment>
     )
   }
