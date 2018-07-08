8a991ad4bd8d7aff7912220353c2bff0a4ce2d1d
Alex Bogovich <bogovich.alexander@gmail.com>
2018-07-09 00:24:57 +0700

    fix reducer

--- src/store/reduces/auth.js
+++ src/store/reduces/auth.js
@@ -1,11 +1,4 @@
-import {
-  AUTH_FAIL,
-  AUTH_START,
-  AUTH_SUCCESS,
-  SIGN_IN_FAIL,
-  SIGN_IN_START,
-  SIGN_IN_SUCCESS,
-} from "../actions/actionTypes"
+import { AUTH_FAIL, AUTH_START, AUTH_SUCCESS } from "../actions/actionTypes"
 
 const initState = {
   token: null,
@@ -17,14 +10,12 @@ const initState = {
 
 export default (state = initState, action) => {
   switch (action.type) {
-    case SIGN_IN_START:
     case AUTH_START:
       return {
         ...state,
         error: null,
         loading: true,
       }
-    case SIGN_IN_SUCCESS:
     case AUTH_SUCCESS:
       return {
         token: action.idToken,
@@ -33,7 +24,6 @@ export default (state = initState, action) => {
         error: null,
         loading: false,
       }
-    case SIGN_IN_FAIL:
     case AUTH_FAIL:
       return {
         ...state,
